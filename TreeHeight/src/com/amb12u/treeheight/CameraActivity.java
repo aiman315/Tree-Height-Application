51e88797d4278f8439930a78567645e7ea52ed1f
Aiman <aiman.buhlaiqah@gmail.com>
2014-11-02 15:37:40 +0000

    Converting Accelerometer reading to degrees

--- TreeHeight/src/com/amb12u/treeheight/CameraActivity.java
+++ TreeHeight/src/com/amb12u/treeheight/CameraActivity.java
@@ -22,8 +22,10 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.Surface;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,30 +33,30 @@ public class CameraActivity extends Activity implements SensorEventListener {
 
 	//TODO: organize variables (private final, private, public, initialization is on create)
 	private final String TAG = "CameraActivity";
-	
+
 	private final String SELETED_CAMERA_ID_KEY = "selectedCameraId";
 	private final int CAMERA_ID_NOT_SET = -1;
-	
+
 	private final int PHOTO_INTENT = 1000;
-	
+
 	private Uri photoFileUri;
-	
+
 	// Determine existence of cameras
 	private boolean hasCamera = false;
 	private boolean hasFrontCamera = false;
-	
+
 	private Camera selectedCamera;
-	
+
 	// Holds ID values for cameras
 	private int frontFacingCameraId = CAMERA_ID_NOT_SET;
 	private int backFacingCameraId = CAMERA_ID_NOT_SET;
 	private int selectedCameraId = CAMERA_ID_NOT_SET;
-	
+
 	// Accelerometer variables
 	private SensorManager mSensorManager;
-    private Sensor mAccelerometer;
+	private Sensor mAccelerometer;
 
-    
+
 
 	/**
 	 * Starts Camera application to capture an image with timeStamp name
@@ -73,7 +75,7 @@ public class CameraActivity extends Activity implements SensorEventListener {
 	protected void onActivityResult (int requestCode, int resultCode, Intent resultIntent) {
 		Log.d(TAG, "onActivityResult");
 		Log.d(TAG, String.format("requestCode: %d | resultCode: %d", requestCode, resultCode));
-		
+
 		if (resultCode == RESULT_CANCELED) {
 			Toast.makeText(this, "User Canceled", Toast.LENGTH_SHORT).show();
 			return;
@@ -104,7 +106,7 @@ public class CameraActivity extends Activity implements SensorEventListener {
 		}
 		openSelectedCamera();
 	}
-	
+
 	/**
 	 * Stops the camera preview
 	 * @param v: The view that invoked the method
@@ -114,14 +116,14 @@ public class CameraActivity extends Activity implements SensorEventListener {
 		releaseSelectedCamera();
 		selectedCameraId = CAMERA_ID_NOT_SET;
 	}
-	
+
 	/** 
 	 * Get the photo directory for application, and creates it if necessary
 	 * @return File outputDir
 	 */
 	private File getPhotoDirectory() {
 		Log.d(TAG, "getPhotoDirectory");
-		
+
 		File outputDir = null;
 		String externalStorageState = Environment.getExternalStorageState();
 		if (externalStorageState.equals(Environment.MEDIA_MOUNTED)) {
@@ -136,26 +138,26 @@ public class CameraActivity extends Activity implements SensorEventListener {
 		}
 		return outputDir;
 	}
-	
+
 	/**
 	 * Creates a time stamp for captured image and converts it to full Uri
 	 * @return photoFileUri
 	 */
 	private Uri generateTimeStampPhotoFileUri() {
 		Log.d(TAG, "generateTimeStampPhotoFileUri");
-		
+
 		Uri photoFileUri = null;
 		File outputDir = getPhotoDirectory();
-		
+
 		if (outputDir != null) {
 			String timeStamp = 	new SimpleDateFormat("dd-MM-yyyy_HH-mm-ss", Locale.UK).format(new Date()); 
 			Toast.makeText(this, timeStamp, Toast.LENGTH_SHORT).show();
 			String photoFileName = "IMG_" + timeStamp + ".jpg";
-			
+
 			File photoFile = new File (outputDir, photoFileName);
 			photoFileUri = Uri.fromFile(photoFile);	
 		}
-		
+
 		return photoFileUri;
 	}
 
@@ -167,7 +169,7 @@ public class CameraActivity extends Activity implements SensorEventListener {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setTitle("No Camera");
 		builder.setMessage("Device does not have required camera supprt. " +
-		"Some features will not be available.");
+				"Some features will not be available.");
 		builder.setPositiveButton("continue", null);
 		AlertDialog dialog = builder.create();
 		dialog.setCanceledOnTouchOutside(false);
@@ -182,10 +184,10 @@ public class CameraActivity extends Activity implements SensorEventListener {
 	private int getFacingCameraId(int facing) {
 		Log.d(TAG, "getFacingCameraId");
 		int cameraId = CAMERA_ID_NOT_SET;
-		
+
 		int nCameras = Camera.getNumberOfCameras();
 		Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
-		
+
 		for(int cameraInfoId = 0 ; cameraInfoId < nCameras ; cameraInfoId++) {
 			Camera.getCameraInfo(cameraInfoId, cameraInfo);
 			// is camera in list of cameras in device the same as the one requested?
@@ -228,13 +230,13 @@ public class CameraActivity extends Activity implements SensorEventListener {
 	private void openSelectedCamera() {
 		Log.d(TAG, "openSelectedCamera");
 		releaseSelectedCamera(); //to ensure no cameras are open
-		
+
 		if(selectedCameraId != CAMERA_ID_NOT_SET) {
 			String message;
 			try {
 				selectedCamera = Camera.open(selectedCameraId);
 				message = "Opened Camera ID:" + selectedCameraId;
-				
+
 				CameraPreview cameraPreview = (CameraPreview) findViewById(R.id.cameraPreview);
 				cameraPreview.connectCamera(selectedCamera, selectedCameraId);
 			} catch (Exception e) {
@@ -244,7 +246,7 @@ public class CameraActivity extends Activity implements SensorEventListener {
 			Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
 		}
 	}
-	
+
 	/**
 	 * Releases the selected camera object, and resets it
 	 */
@@ -257,37 +259,80 @@ public class CameraActivity extends Activity implements SensorEventListener {
 			selectedCamera = null;
 		}
 	}
-	
-	
+
+
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 		Log.d(TAG, "onAccuracyChanged");
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		Log.d(TAG, "onSensorChanged");
-		
+
 		//TODO: remove TextViews. Only need to capture the values
 		TextView textX = (TextView)findViewById(R.id.textViewX);
 		TextView textY = (TextView)findViewById(R.id.textViewY);
 		TextView textZ = (TextView)findViewById(R.id.textViewZ);
-
+		TextView textAngle = (TextView)findViewById(R.id.textViewAngle);
+		
 		float valueX = event.values[0];
 		float valueY = event.values[1];
 		float valueZ = event.values[2];
 		
+		double angle = 0;
+		
+		int rotation = getRotation(this);
+		switch (rotation) {
+		case Surface.ROTATION_0:
+			//portrait
+			angle = calculateAngle(valueY, valueZ);
+			break;
+		case Surface.ROTATION_90:
+			//landscape
+			angle = calculateAngle(valueX, valueZ);
+			break;
+		case Surface.ROTATION_180:
+			//reverse portrait
+			//angle = calculateAngle();
+			break;
+		default:
+			//reverse landscape
+			//angle = calculateAngle();
+			break;
+		}
+		
+		//TODO: remove if not needed
+		/*if(valueY < 0) {
+			Toast.makeText(this, "You have exceeded angle value", Toast.LENGTH_SHORT).show();
+		}*/
+		
 		textX.setText(Float.toString(valueX));
 		textY.setText(Float.toString(valueY));
 		textZ.setText(Float.toString(valueZ));
-		
+		textAngle.setText(Double.toString(angle));
+
 	}
-	
-	
-//	---------------- Activity Methods ---------------- //
-	
+
+	private double calculateAngle(float axis, float depth) {
+		return (Math.atan(depth/axis))* 180 / Math.PI;
+	}
+
+	/**
+	 * Get the current rotation of device
+	 * @param context
+	 * @return (0: portrait, 90: landscape, 180: reverse portrait, 270: reverse landscape)
+	 */
+	private int getRotation(Context context){
+		return ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getRotation();
+	}
+
+
+
+	//	---------------- Activity Methods ---------------- //
+
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
@@ -301,12 +346,12 @@ public class CameraActivity extends Activity implements SensorEventListener {
 		Log.d(TAG, "onCreate");
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_camera);
-		
+
 		//check for camera feature
 		PackageManager pm = getPackageManager();
 		hasCamera = pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
 		hasFrontCamera = pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT);
-		
+
 		if (!(hasCamera || hasFrontCamera)) {
 			showNoCameraDialog();
 		} else {
@@ -323,7 +368,7 @@ public class CameraActivity extends Activity implements SensorEventListener {
 		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-		
+
 	}
 
 	@Override
