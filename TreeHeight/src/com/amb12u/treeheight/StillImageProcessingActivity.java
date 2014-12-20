693861519e2d8aca12e1516213eaec325b8add0c
Aiman <aiman.buhlaiqah@gmail.com>
2014-12-20 15:44:14 +0000

    Added functions for buttons
    Receives the image from previous avtivity

--- TreeHeight/src/com/amb12u/treeheight/StillImageProcessingActivity.java
+++ TreeHeight/src/com/amb12u/treeheight/StillImageProcessingActivity.java
@@ -1,20 +1,65 @@
 package com.amb12u.treeheight;
 
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 public class StillImageProcessingActivity extends Activity {
 	
 	private final String TAG = "StillImageProcessingActivity";
-
+	private Mat imageMat;
+	
+	public void onClickCalculateHeight(View v) {
+		Log.d(TAG, "onClickCalculateHeight");
+		//TODO:
+	}
+	
+	public void onClickDetectReference(View v) {
+		Log.d(TAG, "onClickDetectReference");
+		//TODO:
+	}
+	
+	public void onClickDetectTree(View v) {
+		Log.d(TAG, "onClickDetectTree");
+		//TODO: Currently detecting edges
+		Mat outputMat = new Mat(imageMat.rows(), imageMat.cols(), CvType.CV_8UC4);
+		
+		Imgproc.Canny(imageMat, outputMat, 300, 600, 5, true);
+		Bitmap image = Bitmap.createBitmap(outputMat.cols(), outputMat.rows(), Bitmap.Config.ARGB_8888);
+		Utils.matToBitmap(outputMat, image);
+		
+		ImageView imageView = (ImageView) findViewById(R.id.imageViewCapturedImage);
+		imageView.setImageBitmap(image);
+	}
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		Log.d(TAG, "onCreate");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_still_image_processing);
+		
+		byte[] imageArray = getIntent().getByteArrayExtra("CapturedImage");
+        if (imageArray != null) { 
+        	Bitmap capturedImage = BitmapFactory.decodeByteArray(imageArray, 0, imageArray.length);
+        	
+        	ImageView imageView = (ImageView) findViewById(R.id.imageViewCapturedImage);
+        	imageView.setImageBitmap(capturedImage);
+        
+        	imageMat = new Mat(capturedImage.getHeight(), capturedImage.getWidth(), CvType.CV_8UC4);
+			Utils.bitmapToMat(capturedImage, imageMat);
+        }
 	}
 
 	@Override
