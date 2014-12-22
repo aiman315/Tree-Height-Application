0a7f5f40aceb6508c89b53239b6c72fce6ae1f22
Aiman <aiman.buhlaiqah@gmail.com>
2014-12-22 15:29:09 +0000

    Removed unwanted import

--- TreeHeight/src/com/amb12u/treeheight/ImageProcessingActivity.java
+++ TreeHeight/src/com/amb12u/treeheight/ImageProcessingActivity.java
@@ -20,7 +20,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 
 public class ImageProcessingActivity extends Activity implements CvCameraViewListener2{
