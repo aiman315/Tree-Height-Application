f0b1d93dd7a3fd99a2220f901d83d3e380dda307
Aiman <aiman.buhlaiqah@gmail.com>
2015-03-23 13:15:34 +0000

    Documentation

--- TreeHeight/src/com/amb12u/treeheight/IntStageListener.java
+++ TreeHeight/src/com/amb12u/treeheight/IntStageListener.java
@@ -1,5 +1,10 @@
 package com.amb12u.treeheight;
 
+/**
+ * Listener interface for detection of change in value
+ * @author Aiman
+ *
+ */
 public interface IntStageListener {
 	void onStageChanged();
 }
--- TreeHeight/src/com/amb12u/treeheight/ProgramStage.java
+++ TreeHeight/src/com/amb12u/treeheight/ProgramStage.java
@@ -1,7 +1,11 @@
 package com.amb12u.treeheight;
 
 
-
+/**
+ * Class to hold programme stage
+ * @author Aiman
+ *
+ */
 public class ProgramStage{
 
 	private int stage;
@@ -19,7 +23,7 @@ public class ProgramStage{
 		this.stage = stage;
 		if (stageListener != null) {
 			stageListener.onStageChanged();
-        }
+		}
 	}
 
 	public int getStage() {
@@ -28,5 +32,27 @@ public class ProgramStage{
 
 	public void setListener(IntStageListener stageListener) {
 		this.stageListener = stageListener;
+	}
+
+	public String toString() {
+		String stageString = "Stage is (";
+		switch (stage) {
+		case 0:
+			stageString += "STAGE_HEIGHT_INPUT)";
+			break;
+		case 1:
+			stageString += "STAGE_TREETOP_ANGLE)";
+			break;
+		case 2:
+			stageString += "STAGE_TREE_BOTTOM_ANGLE)";
+			break;
+		case 3:
+			stageString += "STAGE_CALCULATE_TREE_HEIGHT)";
+			break;
+		default:
+			stageString += "not recognised)";
+			break;
+		}
+		return stageString;
 	}
 }
