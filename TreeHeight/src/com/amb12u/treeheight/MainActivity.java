05db59e326c263c3c2485c12ec9cd7a5e607aa69
Aiman <aiman.buhlaiqah@gmail.com>
2015-03-24 15:43:41 +0000

    Added instruction dialog (to select photo source) for image processing activity

--- TreeHeight/src/com/amb12u/treeheight/MainActivity.java
+++ TreeHeight/src/com/amb12u/treeheight/MainActivity.java
@@ -1,8 +1,7 @@
 package com.amb12u.treeheight;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,6 +9,8 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.Toast;
 
 
@@ -67,19 +68,33 @@ public class MainActivity extends Activity {
 		super.onStop();
 	}
 
+	/**
+	 * Handle click on math button, and launches math activity
+	 * @param v
+	 */
 	public void onClickMath(View v) {
 		Log.d(TAG, "onClickMath");
 		Intent cameraIntent = new Intent(this, MathActivity.class);
 		startActivity(cameraIntent);
 	}
 
+	/**
+	 * Handle click on Image Processing button, displays a dialog to allow user to select photo source (Gallery | Camera) and launches Image Processing activity
+	 * @param v
+	 */
 	public void onClickImageProcessing(View v) {
 		Log.d(TAG, "onClickImageProcessing");
 
-		AlertDialog.Builder photoSourceDialog = new AlertDialog.Builder(this);
-		photoSourceDialog.setMessage("Select Photo source");
-		photoSourceDialog.setPositiveButton("Gallery", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface arg0, int arg1) {
+		Dialog dialogPhotoSource = new Dialog(this, R.style.myInstructionDialog);
+		dialogPhotoSource.setContentView(R.layout.dialog_custom_ip_photo_source);
+		dialogPhotoSource.setTitle("Where is the tree?");
+		
+		Button buttonGallery = (Button) dialogPhotoSource.findViewById(R.id.buttonGallery);
+		Button buttonCamera = (Button) dialogPhotoSource.findViewById(R.id.buttonCamera);
+
+		buttonGallery.setOnClickListener( new OnClickListener() {	
+			@Override
+			public void onClick(View v) {
 				Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT, null);
 				galleryIntent.setType("image/*");
 				galleryIntent.putExtra("return-data", true);
@@ -87,13 +102,15 @@ public class MainActivity extends Activity {
 			}
 		});
 
-		photoSourceDialog.setNegativeButton("Camera", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface arg0, int arg1) {
+		buttonCamera.setOnClickListener( new OnClickListener() {
+			@Override
+			public void onClick(View v) {
 				Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 				startActivityForResult(cameraIntent,REQUEST_CODE_CAMERA);
 			}
 		});
-		photoSourceDialog.show();
+		
+		dialogPhotoSource.show();
 	}
 
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
